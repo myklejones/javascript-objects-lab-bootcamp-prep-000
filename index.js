cb3730ee362dfe70d6e4dd241a4299b4069cca15
Mykle jones <mykle.j91@gmail.com>
2019-07-11 16:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-//
-var recipes = {};
-// 
-function updateObjectWithKeyAndValue(object, key, value){
- return  Object.assign({},object, {[key]:value});
-   }
-//
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- object[key]=value;
-  return object; 
-}
-//
- function deleteFromObjectByKey(object, key){
- const newobj = Object.assign({}, object);
- 
-  delete newobj[key];
-  
-  return newobj;
- }
- //
-   function destructivelyDeleteFromObjectByKey(object, key){
-     delete object[key];
-     
-     return object;
-     
-   }
-   
+
   
    
  
