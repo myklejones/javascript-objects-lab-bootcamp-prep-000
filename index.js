47c70edbdbc8ec85d9ad2eac6807e2da1570c4bd
Mykle jones <mykle.j91@gmail.com>
2019-07-11 16:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
  function deleteFromObjectByKey(object, key){
  const newobj = Object.assign({}, object)
-   return object
+  delete newobj[key]
  }
  
  //Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):TypeError: Cannot read property 'prop' of undefined
